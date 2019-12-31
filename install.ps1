9351f9afb16d79e268d9e0711d5e055332d05b3e
martinlinkhorst <martin.linkhorst@gmail.com>
2019-12-31 11:44:31 +0100

    Installation files for win10

---
+++ install.ps1
@@ -0,0 +1,25 @@
+# Install Chocolatey
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Set-ExecutionPolicy Bypass -Scope Process -Force
+
+choco install googlechrome -y
+choco install vscode -y
+choco install kubernetes-cli -y
+choco install kubernetes-helm -y
+choco install python3 -y
+choco install visualstudio2019community -y	
+choco install microsoft-windows-terminal -y
+choco install dotnetcore-sdk -y
+choco install git -y
+choco install azure-cli -y
+choco install wsl -y
+choco install nodejs -y
+choco install nvm -y
+choco install slack -y
+choco install keybase -y
+choco install yarn -y
+choco install androidstudio -y
+choco install keepass -y
+# reboot
+choco install wsl-ubuntu-1804 -y
+
---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Install common bins
+apt-get update \
+      && apt-get install -y apt-transport-https \
+      && apt-get install -y gnupg2 \
+      && apt-get install jq -y
+
+# Install helm
+wget https://get.helm.sh/helm-v3.0.0-linux-amd64.tar.gz
+tar -zxvf helm-v3.0.0-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
+
+# Install terraform
+wget --quiet https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip \
+  && unzip terraform_0.12.13_linux_amd64.zip \
+  && mv terraform /usr/bin
+
+# Install kubectl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get install -y kubectl
+
+# Install .net core
+wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+apt-get update
+apt-get install dotnet-sdk-3.1 -y
+
+# Install az
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+
+# Install VS Code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt-get update
+apt-get install code -y
+
+# Install Zsh
+apt-get install zsh -y
+apt-get install powerline fonts-powerline -y
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s $(which zsh)
+echo "alias k=kubectl" >> ~/.zshrc
+echo "bindkey -v" >> ~/.zshrc
+
+## Add user?
